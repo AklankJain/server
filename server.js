ac08513faeb21c81da8c4b4ceb0f3a878646ca99
Aklank Jain <ajain9@yodlee.com>
2018-03-21 00:08:55 +0530

    getting the code to run

---
+++ server.js
@@ -0,0 +1,18 @@
+
+var express = require('express');
+var app = express();
+
+ // Our first route
+ app.get('/', function (req, res) {
+   res.send('Hello Dev!');
+ });
+
+ // Listen to port 5000
+ app.listen(5000, function () {
+   console.log('Dev app listening on port 5000!');
+ });
+
+ // Our second route
+  app.get('/dev', function (req, res) {
+     res.send('Hello, you are now on the Dev route!');
+  })
